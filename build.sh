1d927267f09b7cf70a78b728bf41a3d5c3dc0c6f
Alex McCausland <alex.mccausland@gmail.com>
2018-08-17 03:49:48 -0400

    Package upgrades

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cp src/integrations/firefox/manifest.json dist/
 ./node_modules/.bin/ajv compile -s src/schemas/state.json -o src/schemas/validate_state.js
 ./node_modules/.bin/rollup -c rollup.background.config.js
 ./node_modules/.bin/rollup -c rollup.main.config.js
-cp -r bower_components/fira/woff dist/assets/css/woff
+cp -r bower_components/fira/woff dist/css/woff
