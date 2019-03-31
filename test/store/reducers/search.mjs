6afe948d30975e0f91c99d9554749f238c2ca74b
Alex McCausland <alex.mccausland@gmail.com>
2019-03-31 16:53:03 -0400

    Fix search test for about urls

--- test/store/reducers/search.mjs
+++ test/store/reducers/search.mjs
@@ -22,13 +22,13 @@ function testSingleWindowSearch( t ) {
       createWindow( window_id, [
         createPinnedTabGroup( [] ),
         createTabGroup( 2, [
-          createTestTab({ id: 4, title: "This is a test" }),
-          createTestTab({ id: 5 }),
-          createTestTab({ id: 6, title: "Another Test" })
+          createTestTab({ id: 4, title: "This is a test", url: "http://example.com" }),
+          createTestTab({ id: 5, url: "http://example.com" }),
+          createTestTab({ id: 6, title: "Another Test", url: "http://example.com" }),
         ]),
         createTabGroup( 3, [
-          createTestTab({ id: 7, title: "Test at the start" }),
-          createTestTab({ id: 8 })
+          createTestTab({ id: 7, title: "Test at the start", url: "http://example.com" }),
+          createTestTab({ id: 8, url: "http://example.com" }),
         ])
       ])
     ]
@@ -37,10 +37,10 @@ function testSingleWindowSearch( t ) {
   let search_text = "test"
   const state1 = startWindowSearch( state0, { window_id, search_text } )
 
-  t.same( state1.windows[ 0 ].search, { text: search_text, resolved: false, matched_tab_ids: [ 4, 6, 7 ], queued_tab_ids: [ 5, 8 ] })
+  t.same( state1.windows[ 0 ].search, { text: search_text, resolved: false, matched_tab_ids: [ 4, 6, 7 ], queued_tab_ids: [ 5, 8 ] } )
 
   let matched_tab_ids = [ 8 ]
-  const state2 = finishWindowSearch( state1, { window_id, search_text, matched_tab_ids })
+  const state2 = finishWindowSearch( state1, { window_id, search_text, matched_tab_ids } )
 
   t.same( state2.windows[ 0 ].search, { text: search_text, resolved: true, matched_tab_ids: [ 4, 6, 7, 8 ], queued_tab_ids: [] } )
 
